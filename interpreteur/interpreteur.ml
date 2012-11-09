cba4eab0a50864be206a79a8822ee10bc6557531
KailasDev <kailas3476@gmail.com>
2012-11-09 02:16:37 +0100

    Update interpreteur/interpreteur.ml

--- interpreteur/interpreteur.ml
+++ interpreteur/interpreteur.ml
@@ -2,17 +2,17 @@ open List;;
 
 let li = 
 ["if";
- " (";
+ "(";
  "(";
  "(";
  "7";
- " +";
+ "+";
  "2";
  ")";
  "*";
- " 2";
+ "2";
  ")";
- " >";
+ ">";
  "(";
  "5";
  "+";
@@ -27,17 +27,14 @@ let li =
  "5";
  ")";
  "then";
- "return";
  "3";
  ";";
  "else";
- "return";
  "2";
  ";";
  "endif";
  ";";
  "else";
- "return";
  "1";
  ";";
  "endif";
@@ -50,48 +47,88 @@ let li3 = ["("; "10"; "*"; "2"; ")"];;
 let li4 = ["("; "3"; "-";"("; "10"; "*"; "2"; ")";")"];;
 let li5 = ["fonction"; "fois";"(";"end";";"];;
 
+
+
 let li6 = ["Fonction"; "fois"; "("; "int"; "X"; ","; "int"; "Y"; ")";
-	   "return"; "("; "X"; "*"; "Y"; ")"; ";"; "END"];;
-
-let string list list li7 = [[""]];;
-
-
-
-
+	   "return"; "("; "X"; "*"; "Y"; ")"; ";";
+	   "END"; ";";
+	   "Fonction"; "fois2"; "("; "int"; "X"; ","; "int"; "Y"; ")";
+	   "return"; "("; "X"; "*"; "Y"; ")"; ";";
+	   "END"; ";"];;
 
 
+(** Recupere le x+1eme element (fonctionne comme tableau) **) 
 
-
-
-
-(***********************************************************)
-(*******************FONCTIONS*NON*DEMANDE*******************)
-
-let rec getPlacCaractSys : string list * string * int -> int = fun(li, s, a) ->
-  if(getCaractList(li, 0) != s)then
-    getPlacCaractSys(getModList(li, 1), s, a+1)
-  else
-    a+1
-;;
-
-let getPlacCaract : string list * string -> int = fun(li, s) ->
-  getPlacCaractSys(li, s, 0)
+let rec getCaractList : string list * int -> string = fun(li, a) ->
+  if(a > 0)then
+    getCaractList(tl(li), a-1)
+  else 
+    hd(li)
 ;;
 
 
+(** Recupere les elements de la liste a partir de l'element a **)
 
-let consStr : string list * string -> string list = fun(li, element) ->
-  element :: li
+let rec getModList : string list * int -> string list = fun(li, a) ->
+  if(a > 0)then
+    getModList(tl(li), a-1)
+  else 
+    li
 ;;
 
 
 
-let consList : string list * string list -> string list = fun(li1, li2) ->
-  li1@li2
+(** Retourne bool pour savoir si exact 
+ ** Modifie pour qu'il trouve la parenthese si y a un if devant **)
+
+let rec getBool : string list -> bool = fun(li) ->
+  if(getCaractList(li, 0) = "(")then
+    if(getCaractList(li, 2) = "<")then
+      if(int_of_string(getCaractList(li, 1)) < int_of_string(getCaractList(li, 3)))then
+	true
+      else  
+	false
+    else  if(getCaractList(li, 2) = ">")then
+      if(int_of_string(getCaractList(li, 1)) > int_of_string(getCaractList(li, 3)))then
+	true
+      else  
+	false
+    else if(getCaractList(li, 10) = "<")then
+      if(sti(setCalcul(getModList(li, 2))) < sti(setCalcul(getModList(li, 12))))then
+	true
+      else 
+	false
+    else if(getCaractList(li, 10) = ">")then
+      if(sti(setCalcul(getModList(li, 2))) > sti(setCalcul(getModList(li, 12))))then
+	true
+      else 
+	false
+    else 
+      false
+  else 
+    getBool(getModList(li, 1))
 ;;
 
-let consListList : string list list * string list -> string list list = fun(li1, li2) ->
-  li2 :: li1
+
+(** retourne le la suite de la liste apres l'element else voulu (systeme) **)
+
+let rec getElseSystem : string list * int -> string list = fun(li, a) ->
+  if(getCaractList(li, 0) = "if") then
+    getElseSystem(getModList(li, 1), a+1)
+      
+  else if(getCaractList(li, 0) = "else")then
+    if(a = 1)then
+      getModList(li, 1)
+    else 
+      getElseSystem(getModList(li, 1), a-1)
+  else 
+    getElseSystem(getModList(li, 1), a)
+;; 
+
+
+(** appelle de la fonction getElseSystem(li, a) **)
+let getElse : string list -> string list = fun(li) ->
+  getElseSystem(li, 0)
 ;;
 
 
@@ -102,6 +139,28 @@ let rec getThen : string list -> string list = fun(li) ->
   else 
     getThen(getModList(li, 1))
 ;;
+
+
+(** recupere la suite de la liste suivant le resultat de l'operation **)
+
+let getOpResult : string list -> string list = fun(li) ->
+  if(getBool(li) = true)then
+    getThen(li)
+  else 
+    getElse(li)
+;;
+
+
+
+(** return du resultat **)
+
+let rec getResult : string list -> string = fun(li) ->
+  if(getCaractList(getOpResult(li),0) = "if")then
+    getResult(getOpResult(li))
+  else 
+    getCaractList(getOpResult(li), 0)
+;;
+
 
 
 (** (int)string (fonction de base trop longue a ecrire) **)
@@ -116,6 +175,41 @@ let sti : string -> int = fun(nb) ->
 let its : int -> string = fun(nb) ->
   string_of_int(nb)
 ;;
+
+
+
+
+
+(** calcule les a et b avec op l'operation **)
+
+let rec setCalculSys : int * int * string -> int = fun(a, b, op) ->
+  if(op = "*")then
+    a * b
+  else if(op = "/")then
+    a / b
+  else if(op = "+")then
+    a + b
+  else
+    a - b
+;;
+
+
+(** systeme du caclul ***)
+
+let rec setCalcul : string list  -> string = fun(li) ->
+  if(getNumPar(li) = 1)then
+    if(getCaractList(li, 2)="*" || getCaractList(li, 2)="/" || getCaractList(li, 2)="+" || getCaractList(li, 2)="-" )then
+      its(setCalculSys(sti(getCaractList(li, 1)), sti(getCaractList(li, 3)), getCaractList(li, 2))) 
+    else
+      "Operateur invalide (operateur possible: +, -, *, /)"
+  else
+    if(getPlacPar(li, 1) = 6)then
+      its(setCalculSys(setCalculSys(sti(getCaractList(li, 2)), sti(getCaractList(li, 4)), getCaractList(li, 3)), sti(getCaractList(li, getPlacPar(li, 1) + 1)), getCaractList(li, getPlacPar(li, 1))))
+    else 
+       its(setCalculSys(sti(getCaractList(li, getPlacPar(li, 1) - 7)), setCalculSys(sti(getCaractList(li, getPlacPar(li, 1) - 4)), sti(getCaractList(li, getPlacPar(li, 1) - 2)), getCaractList(li, getPlacPar(li, 1) - 3)), getCaractList(li, getPlacPar(li, 1)-6)))
+;;
+
+
 
 
 
@@ -158,191 +252,98 @@ let getPlacPar : string list * int -> int = fun(li, b) ->
   getPlacParSys(li, 0, b, 0)
 ;;
 
-let delSpace : string * string -> bool = fun(sOrigine, sCompar) ->
-  delSpaceSys(sOrigine, sCompar, 0, 0)
-;;
 
-let rec delSpaceSys : string * string * int * int -> bool = fun(s, s1, i, j) ->
-  if(String.length(s) >= String.length(s1))then
-    if(j = String.length(s1))then
-      if(i = String.length(s))then
-	true
-      else if(s.[i] = ' ')then
-	delSpaceSys(s, s1, i+1, j)
-      else 
-	false
-    else if(j > 0)then
-      if(s.[i] = s1.[j])then
-	delSpaceSys(s, s1, i+1, j+1)
-      else 
-	false
-    else 
-      if(s.[i] = s1.[j])then
-	delSpaceSys(s, s1, i+1, j+1)
-      else if(s.[i] = ' ')then
-	delSpaceSys(s, s1, i+1, j)
-      else 
-	false
+(**let getBetList : string list * int -> string list = fun(li, a) ->
+  if(a>0)then
+
+  else 
+
+                                                    
+;;**)
+
+
+let rec getPlacCaractSys : string list * string * int -> int = fun(li, s, a) ->
+  if(getCaractList(li, 0) != s)then
+    getPlacCaractSys(getModList(li, 1), s, a+1)
   else
-    false
+    a+1
+;;
 
+let getPlacCaract : string list * string -> int = fun(li, s) ->
+  getPlacCaractSys(li, s, 0)
 ;;
 
 
-(***********************************************************)
-(***********************************************************)
+
+let consStr : string list * string -> string list = fun(li, element) ->
+  element :: li
+;;
+
+let consStr2 : string list list * string list -> string list list = fun(li, element) ->
+  element :: li
+;;
 
 
+let consList : string list * string list -> string list = fun(li1, li2) ->
+  li1@li2
+;;
+
+let rec orderList : string list * int * string list -> string list = fun(li, a, li2)->
+  if( a = length(li))then
+    li2
+  else
+    orderList(li, a+1,consStr(li2, getCaractList(li, a)))
+;;
 
 
+let rec getFunction : string list * string list -> string list = fun(li, li1) ->
+  if(getCaractList(li, 0) = "END")then
+    consStr(consStr(li1, "END"), ";")
+  else
+    getFunction(getModList(li, 1),consStr(li1, getCaractList(li, 0)))
+;;
 
 
+let rec getIndexSys : string list * string * int * int -> int = fun(li, a, i, j)->
+  if(getCaractList(li,0) = a)then
+    i
+  else
+     if(j-1!=0)then
+       getIndexSys(getModList(li, 1), a, i+1, j-1)
+     else
+       i-1
+;;
+
+let getIndex : string list * string -> int = fun(li, a)->
+  getIndexSys(li, a, 0, length(li))
+;;
 
 
+let rec getListFunction : string list * string list list -> string list list = fun(li, li2)->
+  if((getCaractList(li, 0) = "Fonction") && ((getIndex(li, "END")+2) < length(li)))then
+    getListFunction(getModList(li, getIndex(li, "END")+2) ,consStr2(li2, orderList(getFunction(li,[]), 0, [])))
+  else if(getCaractList(li, 0) = "Fonction")then
+    consStr2(li2, orderList(getFunction(li,[]), 0, []))
+  else
+    consStr2(li2, consStr([], getCaractList(li,0)))
+;;
 
-
-(** Recupere le x+1eme element (fonctionne comme tableau) **) 
-
-let rec getCaractList : string list * int -> string = fun(li, a) ->
+let rec getList : string list list * int -> string list = fun(li, a)->
   if(a > 0)then
-    String.trim(getCaractList(tl(li), a-1))
+    getList(tl(li), a-1)
   else 
     hd(li)
 ;;
 
-
-(** Recupere les elements de la liste a partir de l'element a **)
-
-let rec getModList : string list * int -> string list = fun(li, a) ->
-  if(a > 0)then
-    getModList(tl(li), a-1)
+let rec getFunctionListSys : string list list * string *int -> string list = fun(li, s, i)->
+  if(i < 0  && (getCaractList(getList(li, i), 1) != s))then
+    []
+  else if(getCaractList(getList(li, i), 1) = s)then
+    getList(li, i)
   else 
-    li
+    getFunctionListSys(li, s, i-1)
 ;;
 
-
-
-(** Retourne bool pour savoir si exact 
- ** Modifie pour qu'il trouve la parenthese si y a un if devant **)
-
-let rec getBool : string list -> bool = fun(li) ->
-  if(delSpace(getCaractList(li, 0), "("))then
-    if(delSpace(getCaractList(li, 2), "<"))then
-      if(int_of_string(String.trim(getCaractList(li, 1))) < int_of_string(String.trim(getCaractList(li, 3))))then
-	true
-      else  
-	false
-    else  if(delSpace(getCaractList(li, 2), ">"))then
-      if(int_of_string(String.trim(getCaractList(li, 1))) > int_of_string(String.trim(getCaractList(li, 3))))then
-	true
-      else  
-	false
-    else if(delSpace(getCaractList(li, 10), "<"))then
-      if(sti(setCalcul(getModList(li, 2))) < sti((setCalcul(getModList(li, 12)))))then
-	true
-      else 
-	false
-    else if(delSpace(getCaractList(li, 10), ">"))then
-      if(sti(setCalcul(getModList(li, 2))) > sti(setCalcul(getModList(li, 12))))then
-	true
-      else 
-	false
-    else 
-      false
-  else 
-    getBool(getModList(li, 1))
-;;
-
-
-(** retourne le la suite de la liste apres l'element else voulu (systeme) **)
-
-let rec getElseSystem : string list * int -> string list = fun(li, a) ->
-  if(delSpace(getCaractList(li, 0), "if")) then
-    getElseSystem(getModList(li, 1), a+1)
-      
-  else if(delSpace(getCaractList(li, 0), "else"))then
-    if(a = 1)then
-      getModList(li, 1)
-    else 
-      getElseSystem(getModList(li, 1), a-1)
-  else 
-    getElseSystem(getModList(li, 1), a)
-;; 
-
-
-(** appelle de la fonction getElseSystem(li, a) **)
-let getElse : string list -> string list = fun(li) ->
-  getElseSystem(li, 0)
-;;
-
-
-
-(** recupere la suite de la liste suivant le resultat de l'operation **)
-
-let getOpResult : string list -> string list = fun(li) ->
-  if(getBool(li) = true)then
-    getThen(li)
-  else 
-    getElse(li)
-;;
-
-
-
-(** return du resultat **)
-
-let rec getResult : string list -> string = fun(li) ->
-  if(delSpace(getCaractList(getOpResult(li),0), "if"))then
-    getResult(getOpResult(li))
-  else if(delSpace(getCaractList(getOpResult(li), 0), "return"))then
-    getCaractList(getOpResult(li), 1)
-  else 
-    getResult(getModList(li, 1))
-;;
-
-
-(** calcule les a et b avec op l'operation **)
-
-let rec setCalculSys : int * int * string -> int = fun(a, b, op) ->
-  if(op = "*")then
-    a * b
-  else if(op = "/")then
-    a / b
-  else if(op = "+")then
-    a + b
-  else
-    a - b
-;;
-
-
-(** systeme du caclul ***)
-
-let rec setCalcul : string list -> string = fun(li) ->
-  
-  if(getNumPar(li) = 1)then
-    if(String.trim(getCaractList(li, 2))="*" || String.trim(getCaractList(li, 2))="/" || String.trim(getCaractList(li, 2))="+" || String.trim(getCaractList(li, 2))="-" )then
-      its(setCalculSys(sti(String.trim(getCaractList(li, 1))), sti(String.trim(getCaractList(li, 3))), String.trim(getCaractList(li, 2)))) 
-    else
-      "Operateur invalide (operateur possible: +, -, *, /)"
-  else
-    if(getPlacPar(li, 1) = 6)then
-      its(setCalculSys(setCalculSys(sti(String.trim(getCaractList(li, 2))), sti(String.trim(getCaractList(li, 4))), String.trim(getCaractList(li, 3))), sti(String.trim(getCaractList(li, getPlacPar(li, 1) + 1))), String.trim(getCaractList(li, getPlacPar(li, 1)))))
-    else 
-       its(setCalculSys(sti(String.trim(getCaractList(li, getPlacPar(li, 1) - 7))), setCalculSys(sti(String.trim(getCaractList(li, getPlacPar(li, 1) - 4))), sti(String.trim(getCaractList(li, getPlacPar(li, 1) - 2))), String.trim(getCaractList(li, getPlacPar(li, 1) - 3))), String.trim(getCaractList(li, 
-getPlacPar(li, 1)-6))))
-;;
-
-
-
-let rec getFuncSys : string list * int * string list list *string list -> string list list = fun(li, a, lili, li2) ->
-  if(length(li) > a)then
-    if(getCaractList(li, 0) != "END")then
-      getFuncSys(getModList(li, 1), a+1, lili, consStr(li2, getCaractList(li, 0)))
-    else 
-      getFuncSys(getModList(li, 1), a+1, consListList(lili, li), [])
-  else 
-    lili
-;;
-
-let getFunc : string list -> string list list = fun(li) ->
-  getFuncSys(li, 0, [[""]], [])
+let getFunctionList : string list list * string -> string list = fun(li, s)->
+  getFunctionListSys(li, s, length(li)-1)
 ;;
